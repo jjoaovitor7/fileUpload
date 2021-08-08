f8f63bac905d853ad052462121416f117f7ab823
João Vítor <joaovitorsf7@hotmail.com>
2021-08-08 17:24:28 -0300

    minor fix.

--- index.php
+++ index.php
@@ -9,14 +9,14 @@ function sendFile($file, $tmp, $folder, $filenameToServer)
 foreach ($_FILES['file']['name'] as $index => $fileElem) :
     // verificando se o botão foi clicado.
     if (isset($_POST['ok'])) :
-        // pegando a extensão do arquivo
+        // pegando a extensão do arquivo.
         $ext = pathinfo($fileElem, PATHINFO_EXTENSION);
 
-        // verificando se o arquivo não é das extensões .exe, .bat ou .sh
+        // verificando se o arquivo não é das extensões .exe, .bat ou .sh.
         if (!(in_array($ext, array("exe", "bat", "sh")))) :
             $tmp = $_FILES['file']['tmp_name'][$index];
 
-            // gerando um nome para o arquivo que será armazenado no servidor
+            // gerando um nome para o arquivo que será armazenado no servidor.
             $filenameToServer = uniqid() . "." . $ext;
 
             if (file_exists(__DIR__ . DIRECTORY_SEPARATOR . 'files')) :
