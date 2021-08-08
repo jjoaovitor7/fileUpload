523c7330e0d16218e938919ca568b8567a92c65f
João Vítor <joaovitorsf7@hotmail.com>
2021-08-08 16:53:19 -0300

    fix.

--- index.php
+++ index.php
@@ -25,7 +25,21 @@ foreach ($_FILES['file']['name'] as $index => $fileElem) :
 endforeach;
 ?>
 
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
-    <input type="file" name="file[]" multiple />
-    <input type="submit" value="enviar" name="ok" />
-</form>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File Upload</title>
+</head>
+
+<body>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
+        <input type="file" name="file[]" multiple />
+        <input type="submit" value="enviar" name="ok" />
+    </form>
+</body>
+
+</html>
